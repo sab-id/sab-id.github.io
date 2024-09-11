aef29a719c70a395f8bc31db866e4aa323b3f339
Aris Setiawan <arissetia.m@gmail.com>
2024-09-11 16:05:47 +0700

    fixing deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,22 +3,10 @@ const nextConfig = {
     ...(process.env.NODE_ENV === 'production' ? {
         output: 'export',
         basePath: '/sab-id.github.io',
+        assetPrefix: '/sab-id.github.io/',
     } : {}),
     images: {
         unoptimized: true,
-    },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Permissions-Policy',
-                        value: 'interest-cohort=(), private-state-token-redemption=(), private-state-token-issuance=(), browsing-topics=()'
-                    },
-                ],
-            },
-        ]
     },
 };
 
